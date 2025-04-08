1da81bdfa8f10183ea2122b9733576791787a434
Hugh Morgan <serebrum@gmail.com>
2025-04-08 10:16:55 -0600

    added WaveEqn1DAMR.py, which solves the wave equation in 1D using adaptive mesh refinement. refinement currently pushing dx -> 0

--- EulerFlow/WaveEqn1DAMR.py
+++ EulerFlow/WaveEqn1DAMR.py
@@ -7,7 +7,64 @@ Description: Solve the 1D diffusion equation in cartesian, cylindrical, and sphe
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
+from scipy.interpolate import interp1d
+from scipy.ndimage import gaussian_filter1d
 from EulerFlow.BoundaryConditions import GenerateBCs1D
+
+class AdaptiveGrid:
+    """ interpolating onto adaptive grid """
+    def __init__(self, 
+                 grid: np.array, 
+                 field: np.array, 
+                 epsilon: float=1.0, 
+                 minval: float=1e-4,
+                 smooth: bool=True,
+                 ):
+        ## decomposing and mapping onto uniform grid
+        self.grid = grid
+        # normalize r
+        gridNorm = (grid - grid.min()) / (grid.max() - grid.min())
+
+        # compute gradient magnitude
+        grad = np.abs(np.gradient(field, gridNorm) )
+        if smooth:
+            grad = gaussian_filter1d(grad, sigma=2)
+        weight = grad + epsilon
+
+        # build normalized CDF
+        cdf = np.cumsum(weight)
+        cdf = (cdf - cdf[0]) / (cdf[-1] - cdf[0])
+
+        # invert the cdf to get a refined grid
+        inv_cdf = interp1d(cdf, gridNorm)
+        gridNorm_adaptive = inv_cdf(gridNorm)
+
+        # mapping back onto real grid
+        self.grid_adaptive = (grid.max() - grid.min()) * gridNorm_adaptive + grid.min()
+
+        # enforce minimum grid size
+        dx = np.diff(self.grid_adaptive)
+        if np.any(dx < minval):
+            print(f"[make_adaptive_grid] Warning: grid spacing < dx_min ({minval:.2e}). Switching to semi-uniform.")
+            self.grid_adaptive = grid
+
+    def interp_field(self, field):
+        """ Interpolate field onto adaptive grid """
+        return np.interp(self.grid_adaptive, self.grid, field)
+
+
+    def plot(self, field):
+        """ plotting the new and old fields """
+        field_interp = self.interp_field(field)
+        fig, ax = plt.subplots()
+        ax.plot(self.grid, field, marker="|", label='uniform grid')
+        ax.plot(self.grid_adaptive, field_interp, marker="|", label="adaptive grid")
+        ax.grid(True)
+        ax.legend()
+        ax.set_xlabel("grid")
+        ax.set_ylabel("field")
+        ax.set_title("uniform-vs-adaptive grid")
+    
 
 class WaveEqn:
     def __init__(self,
@@ -19,20 +76,23 @@ class WaveEqn:
                  ):
         """ 1D wave equation in cartesian coordinates """
         ## setting up the ghost grid
-        self.npts = grid.size
-        self.grid   = np.copy( grid )
-        self.dr     = self.grid[1] - self.grid[0]
+        self.npts           = grid.size
+        self.grid           = np.copy( grid )
+        self.grid_adaptive  = self.grid
+
+        ## setting up the ghost grid
+        self.dr             = self.grid[1] - self.grid[0]
         self.ghostGrid       = np.zeros(self.npts + 2)
         self.ghostGrid[1:-1] = self.grid
         self.ghostGrid[0]    = grid[0] - self.dr
         self.ghostGrid[-1]   = grid[-1] + self.dr
         self.u_ghost         = np.zeros_like(self.ghostGrid)
         self.dudt_ghost      = np.zeros_like(self.ghostGrid)
+        
         ## if cylindrical/polar or spherical coordinates are used, ensure that the ghost grid lower bound is greater than zero
         self.order = order
         if (order == 1 or order == 2) and self.ghostGrid[0] <= 0:
             raise Exception(f"For cylindrical/polar or spherical coordinates, the ghost grid must be > 0. Lower bound={self.ghostGrid[0]}.")
-        
         
         ## second order spatial difference
         self.ds  = (self.ghostGrid[2:] - self.ghostGrid[:-2]) / 2
@@ -43,11 +103,26 @@ class WaveEqn:
         self.bc_u = GenerateBCs1D(bc_lower, bc_upper)
         
 
-    def __call__(self, t: np.array, y: np.array):
+    def __call__(self, t: np.array, y: np.array, AMR=True):
         """ Right hand side of the diffusion equation """
         u    = np.copy(y[:self.npts])
         dudt = np.copy(y[self.npts:])
-        self.u_ghost[1:-1] = u
+
+        ## using adaptive mesh refinement to map to new grid
+        if AMR:
+            amr = AdaptiveGrid(self.grid_adaptive, u, epsilon=2.0)
+            self.grid_adaptive = amr.grid_adaptive
+            u_adaptive = amr.interp_field(u)
+            dudt_adaptive = amr.interp_field(dudt)
+        else:
+            u_adaptive = u
+            dudt_adaptive = dudt
+
+        # mapping to the ghost grid
+        self.ghostGrid[1:-1] = self.grid_adaptive
+        self.u_ghost[1:-1] = u_adaptive
+        self.ds  = (self.ghostGrid[2:] - self.ghostGrid[:-2]) / 2
+        self.ds2 = self.ds**2
 
         ## apply boundary conditions
         self.bc_u(self.u_ghost, self.ghostGrid)
@@ -60,7 +135,7 @@ class WaveEqn:
         rhs = term1 - term2
         
         #return rhs
-        return np.concatenate((dudt, rhs))
+        return np.concatenate((dudt_adaptive, rhs))
     
 
 if __name__ == '__main__':
@@ -94,7 +169,11 @@ if __name__ == '__main__':
             ax.plot(rGrid, u_t[:,it], label=f"time={tTemp:.3f}s", marker="|")
     ax.grid(True)
     ax.legend()
-    
+
+    ## comparing the grids using static mesh vs AMR
+    amr = AdaptiveGrid(rGrid, u_t[:,outevery], epsilon=2.0)
+    amr.plot(u_t[:,outevery])
+
     #%% density plots
     extent = [tGrid.min(), tGrid.max(), rGrid.min(), rGrid.max()]
     fig, ax = plt.subplots()
